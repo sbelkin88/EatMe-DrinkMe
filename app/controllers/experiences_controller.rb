da695aa261244bcf6e5ca2b37c75a345035c895f
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-18 19:40:58 -0400

    updated Experiences#index route and view

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,8 +1,9 @@
 class ExperiencesController < ApplicationController
-
 	def show
 		@experience = Experience.find_by(id: params[:id])
 	end
 
-
+  def index
+    @experiences = Experience.all.includes(:dishes)
+  end
 end
