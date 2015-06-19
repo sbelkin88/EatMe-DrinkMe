5e1610aa57f8ba745c399e107728cd726ba7305c
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-19 11:33:55 -0400

    added Experiences#create method

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,15 +10,24 @@ class ExperiencesController < ApplicationController
 
   def new
     @experience = Experience.new
+    @venue = Venue.new
     @experience.dishes.build
   end
 
   def create
-    @experience = Experience.new(experience_params)
-    @dish = @experience.dishes.build(experience_params[:dish])
-    binding.pry
-    if @dish.save && @experience.save
-      redirect_to experiences_path
+    @venue = Venue.new(name: params[:venue], address: "Broadway", city: "New York", state: "NY", zip: "10024", phone: "212-222-2222", website: "www.nyc.com")
+    if @venue.save
+      @experience = User.first.experiences.build(venue: @venue)
+      if @experience.save
+        @dish = @experience.dishes.build(experience_params[:dish])
+        if @dish.save
+          redirect_to experiences_path
+        else
+          render :new
+        end
+      else
+        render :new
+      end
     else
       render :new
     end
@@ -27,6 +36,6 @@ class ExperiencesController < ApplicationController
   private
 
   def experience_params
-    params.require(:experience).permit(:unique_place_id, dishes_attributes: [:id, :title, :pic_url, :review])
+    params.require(:experience).permit(dish: [:id, :title, :pic_url, :review])
   end
 end
