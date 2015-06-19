46211fb88bf768aebb833152146dfff3c78a9026
laurisbernhart <lauris.bern@gmail.com>
2015-06-18 20:01:11 -0400

    add paperclip gem and paperclip migration for dish

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'pg_search'
 
+
+
+gem "paperclip", "~> 4.2"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,8 @@ class Dish < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:title, :review]
   belongs_to :experience
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+  									:default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
