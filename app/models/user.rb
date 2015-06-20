f4309e37447d2dda5977ae0462db3629105d1388
laurisbernhart <lauris.bern@gmail.com>
2015-06-20 12:59:28 -0400

    add associations to models, User and Relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,17 @@ class User < ActiveRecord::Base
   multisearchable :against => :username
   
 	has_many :experiences
+
+	has_many :active_relationships,  class_name:  "Relationship",
+	                                   foreign_key: "follower_id",
+	                                   dependent:   :destroy
+	has_many :passive_relationships, class_name:  "Relationship",
+	                                   foreign_key: "followed_id",
+	                                   dependent:   :destroy
+
+	has_many :following, through: :active_relationships,  source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+	
 end
 
 
