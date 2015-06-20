9e8372840a7e31ed7f2f0c3798011283d5f75f8d
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-20 17:42:50 -0400

    got Google Autocomplete working

--- app/assets/javascripts/geocomplete.js
+++ app/assets/javascripts/geocomplete.js
@@ -1,79 +1,71 @@
+$(document).ready(function(){
+  // $("#new_experience").on('submit', getPlaceInfo);
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
+
+var componentForm = {
+  name: 'short_name',
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  postal_code: 'short_name'
+};
+
+var placeID = ""
+
 function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(-33.8688, 151.2195),
-    zoom: 13
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
 
   var input = /** @type {HTMLInputElement} */(
       document.getElementById('pac-input'));
 
-  var types = document.getElementById('type-selector');
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
   var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
-
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
 
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
     var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Autocomplete's returned place contains no geometry");
-      return;
-    }
-
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);  // Why 17? Because it looks good.
-    }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(35, 35)
-    }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
-    }
-
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
+    console.log(place.place_id);
+    document.getElementById("place_id").value = place.place_id;
+    placeID = place.place_id
   });
+};
 
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  function setupClickListener(id, types) {
-    var radioButton = document.getElementById(id);
-    google.maps.event.addDomListener(radioButton, 'click', function() {
-      autocomplete.setTypes(types);
-    });
-  }
+function getPlaceInfo(){
+  // $.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeID + "&key=AIzaSyD0aJPt5PYuQ1hyDTfhkw84CliWkiNA4nI", dataShower)
 
-  setupClickListener('changetype-all', []);
-  setupClickListener('changetype-address', ['address']);
-  setupClickListener('changetype-establishment', ['establishment']);
-  setupClickListener('changetype-geocode', ['geocode']);
+  // function dataShower(data){
+  //   console.log(data)
+  // }
+  //   console.log(placeID)
+  $.ajax({
+    url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeID + "&key=AIzaSyD0aJPt5PYuQ1hyDTfhkw84CliWkiNA4nI" + "&jsoncallback=getPlaceInfoCallback",
+    dataType: 'json',
+    success: function(response){
+      console.log(response)
+    }
+  });
+};
+
+function getPlaceInfoCallback(response){
+  cookie = JSON.parse(response)
+  console.log(cookie)
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// function fillInAddress(place) {
+//   for (var component in componentForm) {
+//     document.getElementById(component).value = '';
+//     document.getElementById(component).disabled = false;
+//   }
+//   for (var i = 0; i < place.address_components.length; i++) {
+//     var addressType = place.address_components[i].types[0];
+//     if (componentForm[addressType]) {
+//       var val = place.address_components[i][componentForm[addressType]];
+//       document.getElementById(addressType).value = val;
+//     }
+//   }
+//   document.getElementById("name").value = place.name;
+//   document.getElementById("phone").value = place.formatted_phone_number;
+//   document.getElementById("website").value = place.url;
+//   document.getElementById("place_id").value = place.place_id;
+// }
+
+
