7bfc4dea56822c2795138ec916eeca758848813c
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-20 10:30:44 -0400

    trying to get places autocomplete to work

--- app/assets/javascripts/geocomplete.js
+++ app/assets/javascripts/geocomplete.js
@@ -1,53 +1,13 @@
-$(document).ready(function(){
-  $('#autocomplete').geocomplete({
-
-    // Whether to snap geocode search to map bounds.
-    bounds: true,
-    country: null,
-
-    // Might be a selector, an jQuery object or a DOM element
-    map: false,
-
-    // The container that should be populated with data.
-    details: "form",
-
-    // The attribute's name to use as an indicator.
-    detailsAttribute: "name",
-
-    // Automatically selects the highlighted item or the first item from the suggestions list on Enter.
-    autoselect: true,
-
-    // Location to initialize the map on.
-    location: false,
-
-    // Options to pass to the 'google.maps.Map' constructor.
-    mapOptions: {
-
-      // The inital zoom level.
-      zoom: 14,
-
-      // Whether to enable the scrollwheel to zoom the map.
-      scrollwheel: false,
-
-      // The map type.
-      mapTypeId: "roadmap"
-    },
-
-    // The options to pass to the `google.maps.Marker` constructor.
-    markerOptions: {
-
-      // If the marker is draggable.
-      draggable: false
-    },
-
-    // An array containing one or more of the supported types for the places request.
-    types: ['geocode'],
-
-    // Trigger geocode when input loses focus.
-    blur: false,
-
-    // If blur is set to true, choose whether to geocode if user has explicitly selected a result before blur.
-    geocodeAfterResult: false
-
-  });
-});
+google.maps.event.addDomListener(window, 'load', function () {
+            var places = new google.maps.places.Autocomplete(document.getElementById('txtPlaces'));
+            google.maps.event.addListener(places, 'place_changed', function () {
+                var place = places.getPlace();
+                var address = place.formatted_address;
+                var latitude = place.geometry.location.A;
+                var longitude = place.geometry.location.F;
+                var mesg = "Address: " + address;
+                mesg += "\nLatitude: " + latitude;
+                mesg += "\nLongitude: " + longitude;
+                alert(mesg);
+            });
+        });
