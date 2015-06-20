0666f0f42570d01c672a1ab5e07107c4aa3f65b8
laurisbernhart <lauris.bern@gmail.com>
2015-06-20 17:18:14 -0400

    add relationships create and destroy actions

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  
+
 	def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
@@ -11,5 +11,6 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(user)
     redirect_to user
   end
-  
+
+
 end
