c0827b26e20babf7ce44b04dddaa413dced2d6d4
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-20 22:17:04 -0400

    updated search functionality for change in models

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -15,9 +15,13 @@ class ExperiencesController < ApplicationController
           @experiences << object.searchable.experience unless @experiences.include?(object.searchable.experience)
         elsif object.searchable_type == "User"
           @experiences += object.searchable.experiences
+        elsif object.searchable_type == "Experience"
+          @experiences << object.searchable
         end
       end
-      @experiences += Experience.venue_search(params[:search])
+      Dish.venue_search(params[:search]).each do |object|
+        @experiences << object.experience unless @experiences.include?(object.experience)
+      end
     else
       @experiences = Experience.all.includes(:dishes)
     end
@@ -46,7 +50,7 @@ class ExperiencesController < ApplicationController
     phone = place["result"]["formatted_phone_number"]
     website = place["result"]["website"]
     @venue = Venue.new(name: place["result"]["name"], address: "#{street_num} #{street_address}", city: city , state: state, zip: zip, phone: phone, website: website)
-    @experience = current_user.experiences.build(experience_params[:name])
+    @experience = current_user.experiences.build(name: experience_params[:name])
     @dish = @experience.dishes.build(experience_params[:dish])
     @dish.venue = @venue
     if @venue.save && @experience.save && @dish.save
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,5 +2,6 @@ class Experience < ActiveRecord::Base
   include PgSearch
   belongs_to :user
   has_many :dishes
+  multisearchable :against => :name
   accepts_nested_attributes_for :dishes
 end
