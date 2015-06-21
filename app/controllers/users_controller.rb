33035979b9408f514c483df64a48a12b81c093b2
laurisbernhart <lauris.bern@gmail.com>
2015-06-21 13:38:29 -0400

    fix links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
 	def show 
 		@user = User.find_by(id: params[:id])
 	end
+
+
+	def following
+	  @users = current_user.following
+	  render 'show_follow'
+	end
+
+	def followers
+	  @users = current_user.followers
+	  render 'show_follow'
+	end
+
+
+	def myfeed
+		@experiences = Experience.joins(user: :following)
+		render 'experiences/index'
+		# @followings = current_user.following.includes(:experiences)
+		# @experiences = User.joins(following: )
+		# Category.joins(articles: [{ comments: :guest }, :tags])
+	end
+
 end
