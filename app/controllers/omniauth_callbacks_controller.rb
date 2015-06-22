5aa29a40ee83e573b76fe5813adbb3ccd054a315
Mendel Schneerson <mmschneerson@gmail.com>
2015-06-22 12:06:06 -0400

    finished contorller method for omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_filter :verify_authenticity_token, only: :all
+  # skip_before_filter :verify_authenticity_token, only: :all
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-    # if user.persisted?
+    if user.persisted?
       sign_in_and_redirect(user)
-    # else
-    #   flash.now[:error] = "Could not save client"
-    #   redirect_to new_user_registration_path
-    # end
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   alias_method :facebook, :all
