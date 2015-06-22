9e8057ae96e9acd18acc7adb2bf7b993d84c83d9
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-22 16:37:30 -0400

    add capability to edit dishes and experiences

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,20 +5,22 @@ class Venue < ActiveRecord::Base
   validates :name, :address, :city, :state, :zip, :phone, presence: true
 
   def self.create_venue(place)
+    venue = Venue.find_by(place_id: place)
+    return venue if venue
     key = ENV["GOOGLE_KEY"]
     uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place}&key=#{key}")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-    place = JSON.parse(response.body)
-    street_num = place["result"]["address_components"][0]["long_name"]
-    street_address = place["result"]["address_components"][1]["long_name"]
-    city = place["result"]["address_components"][2]["long_name"]
-    state = place["result"]["address_components"][3]["short_name"]
-    zip = place["result"]["address_components"][5]["long_name"]
-    phone = place["result"]["formatted_phone_number"]
-    website = place["result"]["website"]
-    Venue.new(name: place["result"]["name"], address: "#{street_num} #{street_address}", city: city , state: state, zip: zip, phone: phone, website: website)
+    place_results = JSON.parse(response.body)
+    street_num = place_results["result"]["address_components"][0]["long_name"]
+    street_address = place_results["result"]["address_components"][1]["long_name"]
+    city = place_results["result"]["address_components"][2]["long_name"]
+    state = place_results["result"]["address_components"][3]["short_name"]
+    zip = place_results["result"]["address_components"][5]["long_name"]
+    phone = place_results["result"]["formatted_phone_number"]
+    website = place_results["result"]["website"]
+    Venue.new(name: place_results["result"]["name"], address: "#{street_num} #{street_address}", city: city , state: state, zip: zip, phone: phone, website: website, place_id: place)
   end
 end
--- db/migrate/20150618215959_create_venues.rb
+++ db/migrate/20150618215959_create_venues.rb
@@ -8,6 +8,7 @@ class CreateVenues < ActiveRecord::Migration
       t.string :zip, null:false
       t.string :phone, null:false
       t.string :website
+      t.string :place_id, null:false
       t.timestamps null: false
     end
   end
