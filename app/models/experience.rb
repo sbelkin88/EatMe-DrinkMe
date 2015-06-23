ff7b93792fa554cf561ddd15167c747d0933f1a4
laurisbernhart <lauris.bern@gmail.com>
2015-06-23 00:12:26 -0400

    refactor and working on partials single-experience and single-dish

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,7 @@
 class Experience < ActiveRecord::Base
   include PgSearch
   belongs_to :user
+  
   has_many :dishes, dependent: :destroy
   multisearchable :against => :name
   accepts_nested_attributes_for :dishes
