0d0b900593191d594318428d55f78f143650a071
laurisbernhart <lauris.bern@gmail.com>
2015-06-23 16:27:43 -0400

    minor styiling changes

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
  
  	var owlCarousels =  $('#render-experiences').find('.owl-carousel')
- 	var owlWrappers = $('#render-experiences').find('.owl-wrapper')
 
  	for(var i = 0; i < owlCarousels.length; i++) {
  		$(owlCarousels[i]).owlCarousel({
   	// navigation : true, // Show next and prev buttons
   	slideSpeed : 300,
   	paginationSpeed : 400,
+  	pagination: true,
   	singleItem:true
-
 		});
 
  	}
