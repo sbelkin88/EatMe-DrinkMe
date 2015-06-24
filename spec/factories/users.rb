999c8474a0c8e91be30cdf96e18aa96c897b2326
laurisbernhart <lauris.bern@gmail.com>
2015-06-24 15:32:01 -0400

    start with features

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
     username {Faker::Internet.user_name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(8)}
+    email 'test1@gmail.com'
+    password 'asdfasdf'
     password_confirmation {password}
   end
 
