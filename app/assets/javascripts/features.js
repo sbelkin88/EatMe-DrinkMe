8e0a635b430c618794d19cbfe4523607d2fc589a
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-24 10:39:17 -0400

    implemented changing colors depending on tab

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -13,11 +13,19 @@ $(document).ready(function() {
 
  	};
   $("#navigation-inner-header [href]").each(function() {
-    if (this.href == window.location.href) {
-      var source = "/assets/icons/1434924558_user_people_man_human_head_person_black.svg"
-        // $(this).closest("div").addClass("active-page");
-        $(this).find("img").attr("src", source)
-        // debugger
-        }
-    });
+      if (this.href == window.location.href) {
+        var source_profile = "/assets/icons/1434924558_user_people_man_human_head_person_turq.svg"
+        var source_public = "/assets/icons/1434924592_compass_turq.svg"
+        var source_feed = "/assets/icons/1434924614_home_house_local_turq.svg"
+          if($(this).closest("div").attr("class") == "nav-my-experiences"){
+            $(this).find("img").attr("src", source_profile)
+          } else if ($(this).closest("div").attr("class") == "nav-explore"){
+            $(this).find("img").attr("src", source_public)
+          } else if ($(this).closest("div").attr("class") == "nav-my-feed"){
+            $(this).find("img").attr("src", source_feed)
+          };
+          }
+      });
+
 });
+
