6e7b52f766c5ec4071e22c45be56b30122fb9de7
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-24 10:46:03 -0400

    rename files

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
  	};
   $("#navigation-inner-header [href]").each(function() {
       if (this.href == window.location.href) {
-        var source_profile = "/assets/icons/1434924558_user_people_man_human_head_person_turq.svg"
-        var source_public = "/assets/icons/1434924592_compass_turq.svg"
-        var source_feed = "/assets/icons/1434924614_home_house_local_turq.svg"
+        var source_profile = "/assets/icons/picture-my-profile-turq.svg"
+        var source_public = "/assets/icons/picture-explore-turq.svg"
+        var source_feed = "/assets/icons/picture-my-feed-turq.svg"
           if($(this).closest("div").attr("class") == "nav-my-experiences"){
             $(this).find("img").attr("src", source_profile)
           } else if ($(this).closest("div").attr("class") == "nav-explore"){
