8c96eed2efc26e541f629a8ee5c31e1c3ecf4cce
laurisbernhart <lauris.bern@gmail.com>
2015-06-24 16:19:45 -0400

    user sign in feature test;

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -2,42 +2,33 @@ require 'rails_helper'
 
 feature 'User signs in' do
 
-  given!(:user) { FactoryGirl.create(:user) {} }
+  given!(:user) { User.create(email: 'test1@gmail.com', username: 'asdfasdf', password: 'asdfasdf') }
 
   scenario 'with valid credentials' do
     visit  new_user_session_path
     fill_in 'Email', with: 'test1@gmail.com'
     fill_in 'Password', with: 'asdfasdf' 
-    click_on 'Log In'
+    click_on "Log in"
 
     expect(page).to have_content('Signed in successfully.')
   end
+
+  scenario 'with invalid email' do
+  	visit  new_user_session_path
+    fill_in 'Email', with: 'invalid@gmail.com'
+    fill_in 'Password', with: 'asdfasdf' 
+    click_on "Log in"
+
+    expect(page).to have_content('Invalid email or password.')
+  end
+
+  scenario 'with invalid email' do
+  	visit  new_user_session_path
+    fill_in 'Email', with: 'test1@gmail.com'
+    fill_in 'Password', with: '' 
+    click_on "Log in"
+
+    expect(page).to have_content('Invalid email or password.')
+  end
 end
 
-
-# feature 'Visitor signs up' do
-#   scenario 'with valid email and password' do
-#     sign_up_with 'valid@example.com', 'password'
-
-#     expect(page).to have_content('Sign out')
-#   end
-
-#   scenario 'with invalid email' do
-#     sign_up_with 'invalid_email', 'password'
-
-#     expect(page).to have_content('Sign in')
-#   end
-
-#   scenario 'with blank password' do
-#     sign_up_with 'valid@example.com', ''
-
-#     expect(page).to have_content('Sign in')
-#   end
-
-#   def sign_up_with(email, password)
-#     visit sign_up_path
-#     fill_in 'Email', with: email
-#     fill_in 'Password', with: password
-#     click_button 'Sign up'
-#   end
-# end
