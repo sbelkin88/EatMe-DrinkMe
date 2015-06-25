8fb86f01966c192b320606d99f4bacdaf8288dfa
Samantha Belkin <samantha.belkin@gmail.com>
2015-06-25 14:46:10 -0400

    sort everything by created_at

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,7 @@ class Dish < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:title, :review]
   pg_search_scope :venue_search, :associated_against => {:venue => [:name, :city]}
+  default_scope {order("created_at")}
   belongs_to :experience
   belongs_to :venue
 
